56f38c701d702c3985c85a31cc812041b642e2b7
karansparks <karanhari564@gmail.com>
2023-11-26 20:45:12 +0530

    changes1

--- suprise/scripts/index.js
+++ suprise/scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 const config = {
   birthdate: 'Nov 24, 2023',
-  name: 'PAVI'
+  name: 'Sam'
 };
 
 function hideEverything() {
